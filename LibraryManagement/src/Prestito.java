5c0c8f54222c96434b412d935b8112e4bc642c08
unknown <figliuolo.giovanni@yahoo.it>
2020-02-17 12:15:24 +0100

    Loan class

---
+++ LibraryManagement/src/Prestito.java
@@ -0,0 +1,71 @@
+import java.util.GregorianCalendar;
+
+public class Prestito {
+	private Persona persona;
+	private Libro libro;
+	private Persona membroStaff;
+	private GregorianCalendar inizioPrestito;
+	private GregorianCalendar finePrestito;
+	private boolean prestitoFinito;
+	
+	Prestito(Biblioteca b, Persona p, Libro l, Persona mS, GregorianCalendar inizioP, GregorianCalendar fineP){
+		this.persona = p;
+		this.libro = l;
+		this.membroStaff = mS;
+		this.inizioPrestito = inizioP;
+		this.finePrestito = fineP;
+		prestitoFinito = false;
+		
+		b.libriPrestati.add(l);
+	}
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+	public Persona getMembroStaff() {
+		return membroStaff;
+	}
+
+	public void setMembroStaff(Persona membroStaff) {
+		this.membroStaff = membroStaff;
+	}
+
+	public GregorianCalendar getInizioPrestito() {
+		return inizioPrestito;
+	}
+
+	public void setInizioPrestito(GregorianCalendar inizioPrestito) {
+		this.inizioPrestito = inizioPrestito;
+	}
+
+	public GregorianCalendar getFinePrestito() {
+		return finePrestito;
+	}
+
+	public void setFinePrestito(GregorianCalendar finePrestito) {
+		this.finePrestito = finePrestito;
+	}
+
+	public boolean isPrestitoFinito() {
+		return prestitoFinito;
+	}
+
+	public void setPrestitoFinito(boolean prestitoFinito) {
+		this.prestitoFinito = prestitoFinito;
+	}
+	
+	
+}
