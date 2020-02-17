808d09af4d18e4f62560158b313caa53200d9506
unknown <figliuolo.giovanni@yahoo.it>
2020-02-17 12:22:19 +0100

    Library class

---
+++ LibraryManagement/src/Biblioteca.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Biblioteca {
+	//Insieme dei libri presenti nella biblioteca
+	private ArrayList<Libro> libriInBiblioteca;
+	//Persone che hanno a che fare con la biblioteca, che siano clienti, proprietari o dipendenti
+	private HashMap<String,Persona> persone;
+	//Insieme dei libri prestati
+	private ArrayList<Libro> libriPrestati;
+	
+	Biblioteca(){
+		libriInBiblioteca = new ArrayList<>();
+		persone = new HashMap<>();
+	}
+	
+	public void aggiungiLibro(Libro l) {
+		libriInBiblioteca.add(l);
+	};
+	
+	public ArrayList<Libro> ricercaLibroDaTesto(String titolo_ricerca) {
+		ArrayList<Libro> libriTrovati = new ArrayList<>();
+		for(int i = 0; i<libriInBiblioteca.size(); i++) {
+			if(libriInBiblioteca.get(i).getTitolo().contains(titolo_ricerca)) {
+				libriTrovati.add(libriInBiblioteca.get(i));
+			}
+		}
+		return libriTrovati;
+	}
+	public void aggiungiDipendente(Persona p) {
+		persone.put(p.getCod_fiscale(),p);
+	}
+	public void aggiungiLibroPrestato(Libro l) {
+		libriPrestati.add(l);
+	}
+	public void rimuoviLibroPrestato(Libro l) {
+		libriPrestati.remove(l);
+	}
+	public void stampaListaLibriPresentiInBiblioteca() {
+		System.out.println(libriInBiblioteca);
+	}
+	
+}
