778a676c81149c60a28f9f97f16b762853a4b6e7
unknown <figliuolo.giovanni@yahoo.it>
2020-02-17 12:14:43 +0100

    Book class

---
+++ LibraryManagement/src/Libro.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class Libro {
+	private Integer isbn;
+	private String titolo;
+	private ArrayList<Persona> autori;
+	private GregorianCalendar edizione;
+	private boolean possibilePrestarlo = true;
+	
+	Libro(){
+		autori = new ArrayList<>();
+	}
+	Libro(Integer isbn, String titolo, ArrayList<Persona> autori, GregorianCalendar edizione){
+		this.isbn = isbn;
+		this.titolo = titolo;
+		this.autori = autori;
+		this.edizione = edizione;
+	}
+	
+	public ArrayList<Persona> getAutori() {
+		return autori;
+	}
+	public void inserisciAutore(Persona autore) {
+		autori.add(autore);
+	}
+	public Integer getISBN() {
+		return isbn;
+	}
+	public void setISBN(Integer isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public GregorianCalendar getEdizione() {
+		return edizione;
+	}
+	public void setEdizione(GregorianCalendar edizione) {
+		this.edizione = edizione;
+	}
+	public void prestato() {
+		possibilePrestarlo = false;
+	}
+	public void finePrestito() {
+		possibilePrestarlo = true;
+	}
+	public String toString() {
+		return "|"+titolo+"|"+autori;
+	}
+}
